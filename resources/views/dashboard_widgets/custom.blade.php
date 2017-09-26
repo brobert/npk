1e1292ddc1d1f645de6eecb5c561d2fcf8bc4cbf
rb5169 <rb5169@hurra.com>
2017-09-26 15:35:47 +0200

    show message list in message widget on homePage, added message details view

---
+++ resources/views/dashboard_widgets/custom.blade.php
@@ -0,0 +1,34 @@
+<div class="col-md-2">
+    <!-- START panel -->
+    <div class="panel panel-primary">
+        <!-- panel heading/header -->
+        <div class="panel-heading">
+            <h3 class="panel-title"><i class="ico-quotes-left mr5"></i> Primary</h3>
+            <!-- panel toolbar -->
+            <div class="panel-toolbar text-right">
+                <!-- option -->
+                <div class="option">
+                    <button class="btn demo" data-toggle="panelrefresh"><i class="reload"></i></button>
+                    <button class="btn up" data-toggle="panelcollapse"><i class="arrow"></i></button>
+                    <button class="btn" data-toggle="panelremove" data-parent=".col-md-4"><i class="remove"></i></button>
+                </div>
+                <!--/ option -->
+            </div>
+            <!--/ panel toolbar -->
+        </div>
+        <!--/ panel heading/header -->
+
+        <!-- panel body with collapse capable -->
+        <div class="panel-collapse pull out">
+            <div class="panel-body">
+                Lorem ipsum dolor sit amet, mei essent everti theophrastus an, accusam lucilius vis eu. In mei accusamus efficiendi mediocritatem, eos ex paulo complectitur.
+            </div>
+        </div>
+        <!--/ panel body with collapse capabale -->
+
+        <!-- Loading indicator -->
+        <div class="indicator"><span class="spinner"></span></div>
+        <!--/ Loading indicator -->
+    </div>
+    <!--/ END panel -->
+</div>
